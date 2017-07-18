d0f46be642cbb6a67855f83d328fd4a3375920b7
Rgstaub <rgstaub@gmail.com>
2017-07-18 00:09:04 -0400

    wokred on ui.html and ui.js

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -10,17 +10,26 @@
 
   db = firebase.database();
 
+//===== Event Listeners ==============================================================
+
   // When the weather button is clicked, read the location and update the DB
   $('#submitWeather').on('click', function() {
     event.preventDefault();
-    var myLoc = $('#myLocation').val().trim()
-    db.ref('location').set(myLoc);
-    $('#myLocation').val(null);
+    if ($('#myLocation').val().trim())
+      var myLoc = $('#myLocation').val().trim();
+      var clockOn = $('#clockEnable').val();
+      // db.ref('location').set(myLoc);
+      // db.ref('weatherOn').set(true);
+      $('#myLocation').val(null);
   })
 
-  db.ref().on('value', function(snap) {
-    var sv = snap.val();
-    console.log(sv.location);
-    // Update the weather ap to display the current location
-    $('#currentLoc').text("Current Location: " + sv.location);
-  })
+// When the Clock
+
+//===== Database Listeners ==========================================================
+
+  // db.ref().on('value', function(snap) {
+  //   var sv = snap.val();
+  //   console.log(sv.location);
+  //   // Update the weather ap to display the current location
+  //   $('#currentLoc').text("Current Location: " + sv.location);
+  // })
