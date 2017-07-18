0b6710d81c75809cb1e6e5f081a5bf0debde1653
Rgstaub <rgstaub@gmail.com>
2017-07-18 15:01:54 -0400

    ui is fleshed out and fully synched with the DB

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -7,29 +7,70 @@
     storageBucket: "group-2a92e.appspot.com",
     messagingSenderId: "867800674419"
   };
+  firebase.initializeApp(config);
+  
+  const db = firebase.database();
 
-  db = firebase.database();
+//===== Initialize Page =============================================================
+
+$(document).ready(function() {
+  // Get a snapshot of all the values to apply to the various inputs
+  db.ref().once('value', function(snap) {
+    var sv = snap.val();
+    $('#enableWeather').prop('checked', sv.weatherOn);
+    $('#enableClock').prop('checked', sv.clockOn);
+    $('#enableQuote').prop('checked', sv.quoteOn);
+    $('#digitalAnalogVal').val(sv.clockStyle);
+    $('#milTimeVal').val(sv.digitalClockStyle);
+  })
+
 
 //===== Event Listeners ==============================================================
 
   // When the weather button is clicked, read the location and update the DB
   $('#submitWeather').on('click', function() {
     event.preventDefault();
-    if ($('#myLocation').val().trim())
+    if ($('#myLocation').val().trim()) {
       var myLoc = $('#myLocation').val().trim();
       var clockOn = $('#clockEnable').val();
-      // db.ref('location').set(myLoc);
-      // db.ref('weatherOn').set(true);
+      db.ref('location').set(myLoc);
       $('#myLocation').val(null);
+    }
+    db.ref('weatherOn').set($('#enableWeather').prop('checked'));
   })
 
-// When the Clock
+  // When the Clock submit button is clicked, collect the selections and set them to the DB
+  $('#submitClock').on('click', function() {
+    event.preventDefault();
+    db.ref('clockStyle').set($('#digitalAnalogVal').val());
+    db.ref('digitalClockStyle').set($('#milTimeVal').val());
+    db.ref('clockOn').set($('#enableClock').prop('checked'));
+  })
+
+  // When the Quote submit button is clicked, collect the selections and set them to the DB
+  $('#submitQuote').on('click', function() {
+    event.preventDefault();
+    db.ref('quoteOn').set($('#enableQuote').prop('checked'));
+  })
+
+  // When clock style 'digital' is selected, enable the option for 12-hour / military time
+  $('#digitalAnalogVal').on('change', function() {
+    var style = $('#digitalAnalogVal').val();
+    if (style === "analog") {
+      $('#milTimeVal').prop('disabled', 'disabled');
+    }
+    else if (style === 'digital') {
+      $('#milTimeVal').prop('disabled', false);
+    }
+  })
 
 //===== Database Listeners ==========================================================
 
-  // db.ref().on('value', function(snap) {
-  //   var sv = snap.val();
-  //   console.log(sv.location);
-  //   // Update the weather ap to display the current location
-  //   $('#currentLoc').text("Current Location: " + sv.location);
-  // })
+  db.ref().on('value', function(snap) {
+    var sv = snap.val();
+    console.log(sv.location);
+    // Update the weather ap to display the current location
+    $('#currentLoc').text("Current Location: " + sv.location);
+  })
+
+})
