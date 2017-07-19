13477563b0e96d3a15447b495eca1ac03bdcac19
Taylor Caton <taylorcaton@gmail.com>
2017-07-18 20:56:02 -0400

    catching up to master

--- assets/js/mirror.js
+++ assets/js/mirror.js
@@ -1,8 +1,7 @@
 // Make a new API call and re-draw the weather results when the db changes
 db.ref().on('value', function(snap) {
 
-	console.log("value " + snap.val().locationName)	;
-    getWeather(snap.val().locationName);
+	getWeather(snap.val().location);
 
     if(snap.val().digitalClockStyle === "military"){
     	show24HourTime();
@@ -14,6 +13,8 @@ document.addEventListener("DOMContentLoaded", function() {
 	getQuotes();
 });
 
+
+//Fetches a new quote every 60 seconds
 function getQuotes(){
 	setInterval(getQuote, 60000);
 	getQuote();
