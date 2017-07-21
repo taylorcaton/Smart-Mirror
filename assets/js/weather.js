e36bb8ea3788ba487925b799b7c29c7de051197a
Taylor Caton <taylorcaton@gmail.com>
2017-07-20 21:21:34 -0400

    added high and low

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,15 +1,15 @@
-
-
 var temp;
 var icon;
 var description;
+var hi;
+var low;
 
 //Draws the weather to the window
 function getWeather(location){
 
     var APIKey = "44750aae265346679f0162443170607";
 
-    var queryURL = "https://api.apixu.com/v1/current.json?" +
+    var queryURL = "https://api.apixu.com/v1/forecast.json?" +
     "key="+APIKey+"&q="+location;
     
     $.ajax({
@@ -25,9 +25,17 @@ function getWeather(location){
         temp = ""+data.current.temp_f;
         description = data.current.condition.text;
         icon = data.current.condition.icon;
+        hi = ""+data.forecast.forecastday["0"].day.maxtemp_f;
+        low = ""+data.forecast.forecastday["0"].day.mintemp_f;
 
         if(temp.indexOf(".") > -1){
             temp = temp.substring(0, temp.indexOf(".")); //Remove everthing after the decimal
+        }
+        if(hi.indexOf(".") > -1){
+            hi = hi.substring(0, hi.indexOf(".")); //Remove everthing after the decimal
+        }
+        if(low.indexOf(".") > -1){
+            low = low.substring(0, low.indexOf(".")); //Remove everthing after the decimal
         }
         
         icon = icon.substring(icon.indexOf(".png")-3, icon.indexOf(".png")); //grab the icon prefix
@@ -37,11 +45,16 @@ function getWeather(location){
         var row2 = $("<div class='row text-left'>");
 
         row.append("<div class='col-xs-6'><img src='assets/images/"+icon+".png' alt='Icon depicting current weather.'></div>")
-        row.append("<div class='col-xs-6'><div id='temperature'>" + temp + "<sup style='font-size:32px; top: -1.5em;'>°F</sup></div></div>")
+        row.append( "<div class='col-xs-6'><div class='row'>"+
+                    "<div class='text-center' id='temperature'>" + temp + "<sup style='font-size: 16px; top: -1.5em;'>°F</sup></div>"+
+                    "<div class='row'><div class='col-xs-12'><h2 class='text-center' id='condition'>" + description + "</h2></div>"+
+                    "</div>")
 
         newDiv.append(row);
 
-        row2.append("<div class='col-xs-12'><h2 class='text-center' id='condition'>" + description + "</h2></div>")
+        row2.append("<div class='col-xs-12'><h2 class='text-center' id='hiLo'>" + 
+                    "<div><i class='fa fa-angle-up' aria-hidden='true'></i>" + hi + 
+                    "<div><i class='fa fa-angle-down' aria-hidden='true'></i>" + low + "</h2></div>")
         newDiv.append(row2);
 
         $('#weatherPane').fadeOut('slow', function() {
@@ -87,6 +100,7 @@ function getWeatherUI(location){
         updates["locationName"] = data.location.name+", "+data.location.region;
         updates["location"] = location;
         updates["timezone"] = data.location.tz_id;
+        updates["localTime"] = data.location.localtime;
         db.ref().update(updates)
 
     });
